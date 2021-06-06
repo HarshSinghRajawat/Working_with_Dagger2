c390182ea54835ae19df87ff6c6665db43560223
HarshSinghRajawat <harshsinghrajawat86@gmail.com>
2021-06-06 23:12:14 +0530

    myCommit

--- app/src/main/java/com/one/dummydagger2/Dagger/Component/MobileComponent.java
+++ app/src/main/java/com/one/dummydagger2/Dagger/Component/MobileComponent.java
@@ -1,5 +1,6 @@
 package com.one.dummydagger2.Dagger.Component;
 
+import com.one.dummydagger2.MainActivity;
 import com.one.dummydagger2.Model.Mobile;
 
 import dagger.Component;
@@ -7,4 +8,5 @@ import dagger.Component;
 @Component
 public interface MobileComponent {
     Mobile getMobile();
+    void inject(MainActivity activity);
 }
--- app/src/main/java/com/one/dummydagger2/MainActivity.java
+++ app/src/main/java/com/one/dummydagger2/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mobile= DaggerMobileComponent.create().getMobile();
+        DaggerMobileComponent.create().inject(this);
         mobile.run();
     }
 }
